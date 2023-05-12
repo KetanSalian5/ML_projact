7c52c61389ca86a2b1b6c8e54a6a6f6b821592cb
ketan salian <ketan.salian3@gmail.com>
2023-05-12 16:59:40 +0530

    add new

--- setup.py
+++ setup.py
@@ -2,18 +2,29 @@ from setuptools import find_packages,setup
 from typing import List
 
 HYPEN_E_DOT='-e .'
+#No, the correct syntax for defining the return type in the function signature is using -> The -> arrow indicates the return type of the function.
 def get_requirements(file_path:str)->List[str]:
     '''
     this function will return the list of requirements
     '''
+    #we creat the empty list []
     requirements=[]
+    
+    #This opens the file specified by file_path in a context manager. It ensures that the file is properly closed after reading its contents.
     with open(file_path) as file_obj:
+        
+ #This reads all the lines from the file and stores them as a list of strings in the requirements variable. 
+#Each line in the file becomes an item in the list.       
         requirements=file_obj.readlines()
+#        
         requirements=[req.replace("\n","") for req in requirements]
+        
+#This conditional statement checks if the string stored in HYPEN_E_DOT exists in the requirements list. 
+# If it does, the remove() method is called to remove the occurrence of HYPEN_E_DOT from the list.
 
         if HYPEN_E_DOT in requirements:
             requirements.remove(HYPEN_E_DOT)
-    
+#Finally, the function returns the requirements list, which contains the parsed requirements from the file.    
     return requirements
 
 setup(
--- src/exception.py
+++ src/exception.py
@@ -1,9 +1,11 @@
 """We are writing our now custom exeception"""
 
 import sys
-#from src.logger import logging
+from src.logger import logging
+#The error_details parameter is annotated with the type sys, indicating that it expects an object from the sys module.
 
 def error_massage_detail(error,error_details:sys):
+#_, _, exc_tb = error_details.exc_info(), it is used to discard the first two elements of the tuple returned by error_details.exc_info().
     _,_,exc_tb=error_details.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename  
     error_massage ="Error occured in the python script name [{0}] line number [{1}] error massage [{2}]".format(
@@ -20,4 +22,4 @@ class CustomException(Exception):
         self.error_massage=error_massage_detail(error_massage,error_details=error_detail)
         
     def __str__(self):
-        return self.error_massage
+        return self.error_massage 
